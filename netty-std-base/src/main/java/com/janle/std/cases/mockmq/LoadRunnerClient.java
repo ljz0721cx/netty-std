1d572e6d6ac09a16ba27069264ef9757647d99b3
ljz0721cx <J_janle@163.com>
2019-01-22 21:10:08 +0800

    模拟消息内存溢出

---
+++ netty-std-base/src/main/java/com/janle/std/cases/mockmq/LoadRunnerClient.java
@@ -0,0 +1,40 @@
+package com.janle.std.cases.mockmq;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * Created by lijianzhen1 on 2019/1/22.
+ */
+public class LoadRunnerClient {
+    static final String HOST = System.getProperty("host", "127.0.0.1");
+    static final int PORT = Integer.parseInt(System.getProperty("port", "8080"));
+
+    @SuppressWarnings({"unchecked", "deprecation"})
+    public static void main(String[] args) throws Exception {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group)
+                    .channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY, true)
+                    .option(ChannelOption.WRITE_BUFFER_HIGH_WATER_MARK, 10 * 1024 * 1024)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline p = ch.pipeline();
+                            p.addLast(new LoadRunnerClientHandler());
+
+                        }
+                    });
+            ChannelFuture f = b.connect(HOST, PORT).sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+
+}
---
+++ netty-std-base/src/main/java/com/janle/std/cases/mockmq/LoadRunnerClientHandler.java
@@ -0,0 +1,68 @@
+package com.janle.std.cases.mockmq;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Created by lijianzhen1 on 2019/1/22.
+ */
+public class LoadRunnerClientHandler extends ChannelInboundHandlerAdapter {
+    private final ByteBuf firstMessage;
+    Runnable loadRunner;
+
+    AtomicLong sendSum = new AtomicLong(0);
+    Runnable profileMonitor;
+
+    static final int SIZE = Integer.parseInt(System.getProperty("size", "256"));
+
+    /**
+     * Creates a client-side handler.
+     */
+    public LoadRunnerClientHandler() {
+        firstMessage = Unpooled.buffer(SIZE);
+        for (int i = 0; i < firstMessage.capacity(); i ++) {
+            firstMessage.writeByte((byte) i);
+        }
+    }
+
+    @Override
+    public void channelActive(final ChannelHandlerContext ctx) {
+        loadRunner = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    TimeUnit.SECONDS.sleep(30);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                ByteBuf msg = null;
+                final int len = "Netty OOM Example".getBytes().length;
+                while(true)
+                {
+                    msg = Unpooled.wrappedBuffer("Netty OOM Example".getBytes());
+                    ctx.writeAndFlush(msg);
+                }
+            }
+        };
+        new Thread(loadRunner, "LoadRunner-Thread").start();
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg)
+    {
+        ReferenceCountUtil.release(msg);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+}
